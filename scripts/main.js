2abd0fe441d4d4de27ea1d4bdeec2fa6f80ec71c
AbdulMuiz <abdulmuiz@pop-os.localdomain>
2021-09-16 16:05:23 +0300

    A 100% working dropdown with animation made in the index.htmlwith appropriate modifications to the files

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,43 @@
 // enabling dropdown clickable
-const dropdownButton = document.querySelector("li.dropdown-button");
+const dropdownButton = document.querySelector("a.dropdown-button");
 const dropdownContent = document.querySelector("div.dropdown-content");
 dropdownButton.addEventListener("click", function () {
   if (dropdownContent.classList.contains("show-dropdown")) {
-    dropdownContent.classList.toggle("hide-dropdown");
     dropdownContent.classList.remove("show-dropdown");
+    dropdownContent.classList.add("hide-dropdown");
   }else{
-    dropdownContent.classList.toggle("show-dropdown");
     dropdownContent.classList.remove("hide-dropdown");
+    dropdownContent.classList.add("show-dropdown");
   }
 });
 
 window.addEventListener("click", function (e) {
   if (!e.target.matches(".dropdown-part")) {
-    if (dropdownContent.classList.contains("show-dropdown")) {
-      dropdownContent.classList.toggle("show-dropdown");
-      dropdownContent.classList.toggle("hide-dropdown");
-    }
+    console.log("dropdown-part didnt match")
+    if(!e.target.matches(".dropdown-content")){
+      console.log("dropdown-content didnt match")
+      if(!e.target.matches(".dropdown-links")){
+        console.log("dropdown-links didnt match")
+        if (dropdownContent.classList.contains("show-dropdown")) {
+          dropdownContent.classList.remove("show-dropdown");
+          dropdownContent.classList.add("hide-dropdown");
+        }
   }
+  }
+}
 });
 
 //making the nightmode switch
 const toggleBtn = document.getElementById("toggle-button");
 const sun = document.getElementById("sun");
 const moon = document.getElementById("moon");
-const mainTheme = document.getElementById("style-sheet");
+const darkThemeStylesheet = document.getElementById("dark-theme-css");
 const mainQuote=document.getElementById("index-quote")
 const myPic = document.querySelector("img.muiz-image");
-console.log(mainTheme);
 var isDark = false;
 
 function swapStyleSheets(sheetPath) {
-  mainTheme.setAttribute("href", sheetPath);
+  darkThemeStylesheet.setAttribute("href", sheetPath);
 }
 
 toggleBtn.addEventListener("click", function () {
