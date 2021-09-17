a088fff8191e9b4eb0d36f9d379e2755e3ae7961
Muiz-Jaguar <muiz.jaguar@gmail.com>
2021-09-17 21:50:33 +0300

    v2.0 commit with a working dropdown and nightmode DB

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,7 @@
 const dropdownButton = document.querySelector("a.dropdown-button");
 const dropdownContent = document.querySelector("div.dropdown-content");
 dropdownButton.addEventListener("click", function () {
-  if (dropdownContent.classList.contains("show-dropdown")) {
-    dropdownContent.classList.remove("show-dropdown");
-    dropdownContent.classList.add("hide-dropdown");
-  } else {
-    dropdownContent.classList.remove("hide-dropdown");
-    dropdownContent.classList.add("show-dropdown");
-  }
+  dropdownContent.classList.toggle("show-dropdown")
 });
 
 window.addEventListener("click", function (e) {
@@ -17,7 +11,6 @@ window.addEventListener("click", function (e) {
       if (!e.target.matches(".dropdown-links")) {
         if (dropdownContent.classList.contains("show-dropdown")) {
           dropdownContent.classList.remove("show-dropdown");
-          dropdownContent.classList.add("hide-dropdown");
         }
       }
     }
@@ -31,7 +24,26 @@ const moon = document.getElementById("moon");
 const darkThemeStylesheet = document.getElementById("dark-theme-css");
 const mainQuote = document.getElementById("index-quote");
 const myPic = document.querySelector("img.muiz-image");
-var isDark = false;
+var isDark;
+if(localStorage.getItem("darkMode")){
+  isDark = JSON.parse(localStorage.getItem("darkMode"));
+}else{
+  isDark=false;
+  localStorage.setItem("darkMode","false")
+}
+
+if(isDark){
+  toggleBtn.className = "fas fa-toggle-on switch";
+  sun.className = "far fa-sun sun";
+  moon.className = "fas fa-moon moon";
+  swapStyleSheets("styles/darktheme.css")
+  if (mainQuote) {
+    mainQuote.className = "dark-animated";
+  }
+  if (myPic) {
+    myPic.classList.toggle("darkmode-img-border");
+  }
+}
 
 function swapStyleSheets(sheetPath) {
   darkThemeStylesheet.setAttribute("href", sheetPath);
@@ -41,6 +53,7 @@ toggleBtn.addEventListener("click", function () {
   //checking for darkmode
   if (!isDark) {
     isDark = true;
+    localStorage.setItem("darkMode","true")
     //applying darkmode
     if (isDark) {
       toggleBtn.className = "fas fa-toggle-on switch";
@@ -68,5 +81,6 @@ toggleBtn.addEventListener("click", function () {
       myPic.classList.toggle("darkmode-img-border");
     }
     isDark = false;
+    localStorage.setItem("darkMode","false")
   }
 });
