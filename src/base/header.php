0756cb9decb0d5ddc2e85fdcbecd7717638812f4
Abdullah Bin Rashid <abdullahbinrashid2005abr@outlook.com>
2021-09-17 15:40:16 +0500

    Add header.php and added to about-me.php

---
+++ src/base/header.php
@@ -0,0 +1,33 @@
+<div class="header-content">
+      <a href="/" class="logo" title="homepage">&nbsp;Abdul Muiz .</a>
+      <i class="fas fa-sun sun" id="sun"></i>
+      <i
+        class="fas fa-toggle-off switch"
+        id="toggle-button"
+        title="nightmode"
+      ></i>
+      <i class="far fa-moon moon" id="moon"></i>
+    </div>
+    
+    
+    <div class="navbar-container">
+      <!-- navbar for big screens -->
+      <ul class="navbar">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about-me.html">About Me</a></li>
+        <li><a href="/facts.html">Facts</a></li>
+        <li><a href="/friend-links.html"> My friends </a></li>
+      </ul>
+      <!-- navbar for small screens -->
+      <ul class="small-navbar">
+        <li><a class="fas fa-home navbar-icons" href="index.html"></a></li>
+        <li class="dropdown-part">
+          <a class="fas fa-bars navbar-icons dropdown-button dropdown-part"></a>
+          <div class="dropdown-content">
+            <a href=/facts.html" class="dropdown-links">Facts</a>
+            <a href="/about-me.html" class="dropdown-links">About me</a>
+            <a href="friend-links.html" class="dropdown-links">Friends</a>
+          </div>
+        </li>
+      </ul>
+    </div>
